e1a3d1e47b406befbc293406bf3ddba590e8dbfb
AlexanderPampushko <a.pampushko@altuera.com>
2019-03-29 23:21:24 +0200

    сервлет для автоматической загрузки оригиналов файлов
    после успешной обработки ThreadEmulation создаём копию оригинала файла
    и высылаем пользователю в чат вместе с уведомлением о успешной проверке
    также ссылку на скачивание оригинала файла (ссылка имеет вид http://localhost:8090/getFile?fileId=1553885449363-0)
    1553885449363-0 - имя каталога в который будет помещен файл при создании копии
    каталог создает в базовой директории для хранения копий файлов.
    Базовая директория для хранения копий файлов задается настройкой конфига storageBaseDir

---
+++ src/main/scala/com/altuera/gms_antivirus_service/file_storage/FileDownloadServlet.scala
@@ -0,0 +1,97 @@
+// © LLC "Altuera", 2019
+package com.altuera.gms_antivirus_service.file_storage
+
+import java.io._
+
+import com.altuera.gms_antivirus_service.{Configuration, Utils}
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.apache.commons.io.IOUtils
+import org.slf4j.LoggerFactory
+import spray.json.{JsObject, JsString}
+
+import scala.util.control.NonFatal
+
+@WebServlet(
+  name = "FileDownload",
+  displayName = "FileDownload",
+  description = "FileDownload",
+  urlPatterns = {
+    Array("/getFile/*")
+  },
+  loadOnStartup = 1
+)
+class FileDownloadServlet extends HttpServlet {
+  val defaultEncoding = System.getProperty("file.encoding")
+  private val log = LoggerFactory.getLogger(this.getClass)
+  val baseDirectoryForStorageDirs = Utils.createDirIfNotExist(Configuration.storageBaseDir)
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    try {
+      handle(request, response)
+    } catch {
+      case NonFatal(ex) =>
+        log.error("error", ex)
+        makeResponse(response, ex)
+    }
+  }
+
+  def handle(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    response.reset()
+    val fileId = request.getParameter("fileId")
+    if (!fileId.isEmpty) {
+      val fileFromStorage = Utils.getFileByBaseDirAndSeparateTempDir(baseDirectoryForStorageDirs, fileId)
+      if (fileFromStorage.isPresent) {
+
+        downloadFile(fileFromStorage.get(), response)
+      }
+      else {
+        makeResponse(response, "не найден файл")
+      }
+
+    } else {
+      makeResponse(response, "Файл еще не загружен. Попробуйте позже")
+    }
+  }
+
+  def downloadFile(file: File, response: HttpServletResponse): Unit = {
+    val fileName: String = file.getName
+    response.setCharacterEncoding("utf-8")
+    response.setContentType("application/force-download")
+    //response.setContentType("application/octet-stream")
+    response.setHeader("Content-Length", "" + file.length())
+    response.setHeader("Content-Description", "File Transfer")
+    response.setHeader("Content-Transfer-Encoding", "binary")
+    response.setDateHeader("Expires", 0)
+    response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0")
+    response.setHeader("Pragma", "public")
+
+    if (defaultEncoding != null && defaultEncoding == "UTF-8") response.addHeader("Content-Disposition",
+      "attachment;filename=" + new String(fileName.getBytes("GBK"), "iso-8859-1"))
+    else response.addHeader("Content-Disposition",
+      "attachment;filename=" + new String(fileName.getBytes, "iso-8859-1"))
+    val os = response.getOutputStream
+    val fileInputStream = new FileInputStream(file)
+    IOUtils.copy(fileInputStream, os)
+    fileInputStream.close()
+    os.flush()
+    os.close()
+  }
+
+  private def makeResponse(response: HttpServletResponse, message: String): Unit = {
+    response.resetBuffer
+    response.setStatus(HttpServletResponse.SC_OK)
+    response.setContentType("application/json")
+    response.setCharacterEncoding("UTF-8")
+    response.getOutputStream.print(JsObject("message" -> JsString(message)).toString)
+    response.flushBuffer //marks response as committed -- if we don't do this the request will go through normally!
+  }
+
+  private def makeResponse(response: HttpServletResponse, throwable: Throwable): Unit = {
+    var message = throwable.toString
+    makeResponse(response, message)
+  }
+}
+
+
+
